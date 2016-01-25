10233139f0fa01f79bca48bb8290b921c24def0f
Malcolm Neal <Malcolm259@Gmail.com>
2016-01-25 16:10:44 -0500

    Implemented game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    //Game Time
+    float m_MatchTime = 0.0f;
+    float m_WaitTime = 0.0f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //Points
+    int m_Points_Player1= 0,
+        m_Points_Player2 = 0;
+
+    //State
+
+    public eGameState CurrentGameState = eGameState.Waiting;
+    public enum eGameState
+    {
+        Waiting,
+        Playing
+    }
+
+    public void FixedUpdate( )
+    {
+        if(CurrentGameState == eGameState.Waiting )
+        {
+            m_WaitTime += Time.fixedDeltaTime;
+        }else if(CurrentGameState == eGameState.Playing )
+        {
+            m_MatchTime += Time.fixedDeltaTime;
+        }
+    }
+
+    public void StartGameState(eGameState newGameState )
+    {
+        if( newGameState != CurrentGameState )
+        {
+            //End current gamestate
+            switch ( CurrentGameState )
+            {
+                case eGameState.Waiting:
+                case eGameState.Playing:
+                    break;
+            }
+
+            //Start new gamestate
+            switch ( newGameState )
+            {
+                case eGameState.Waiting:
+                case eGameState.Playing:
+                    break;
+            }
+
+            //reset time
+            m_WaitTime = 0;
+            m_MatchTime = 0;
+        }
+    }
 }
